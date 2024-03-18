1d3fbe0e3deb25d31e462eeccc0757fcefaefb67
Joshua Ketor <joshuaketor@proton.me>
2024-03-18 15:03:21 +0000

    Add: 2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """Replace an element of a list at a specified
+    position (like in C).
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    my_list[idx] = element
+    return my_list
