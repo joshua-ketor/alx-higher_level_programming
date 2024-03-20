7f17089d8ab37d249429f21d7dc7dd5b9d382cf9
Joshua Ketor <joshuaketor@proton.me>
2024-03-20 22:15:43 +0000

    Update: 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,7 +8,8 @@ def roman_to_int(roman_string):
     # variables
     total = 0
     prev_value = 0
-    roman_to_int = {'I': 1, 'V': 5,'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_to_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 1000}
 
     # loop through each letter
     for letter in reversed(roman_string):
