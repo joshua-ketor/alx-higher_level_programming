0bc2a1b2c04cfa0a06efd6d88b8f7940508a02a0
Joshua Ketor <joshuaketor@proton.me>
2024-03-20 18:33:03 +0000

    Update: 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """ return a key with the biggest integer value """
-    if a_dictionary == None or len(a_dictionary) == 0:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     max_num = 0
     for key in list(a_dictionary):
