e196f80fcba5b48328d52a885aa8c198f3424df0
Joshua Ketor <joshuaketor@proton.me>
2024-03-20 18:29:44 +0000

    Add: 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """ return a key with the biggest integer value """
+    if a_dictionary == None or len(a_dictionary) == 0:
+        return None
+    max_num = 0
+    for key in list(a_dictionary):
+        if a_dictionary[key] > max_num:
+            max_num = a_dictionary[key]
+            max_key = key
+    return max_key
