5753ce4d75cd368e7389ef1a16326f358f398d04
Joshua Ketor <joshuaketor@proton.me>
2024-04-02 22:14:12 +0000

    Update: 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     n = 0
-    for el in my_list:
+    for el in range(x):
         try:
-            print(el, end='')
+            print(my_list[el], end='')
             n += 1
         except IndexError:
             break
