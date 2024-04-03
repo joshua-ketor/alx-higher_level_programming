4cab17264a1d040adb114d50e23f1eefbeca8d31
Joshua Ketor <joshuaketor@proton.me>
2024-04-03 00:04:11 +0000

    Add: 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module: square"""
+
+
+class Square:
+    """square"""
+
+    def __init__(self, size=0):
+        """Constructor"""
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        is size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """Area of a square"""
+
+        return self.__size ** 2
