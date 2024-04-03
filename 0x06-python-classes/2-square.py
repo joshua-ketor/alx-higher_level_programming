e4fd4501f42722139d6fdef28c1c52b1c6f083a5
Joshua Ketor <joshuaketor@proton.me>
2024-04-03 17:44:10 +0000

    Add: 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module with a square class"""
+
+
+class Square:
+    """A square class"""
+
+    def __init__(self, size=0):
+        """Instatiation method
+        Args:
+            self (obj): self reference to instance
+            size: size of square
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
