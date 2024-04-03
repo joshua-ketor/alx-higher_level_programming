bbbd815bbe81b60d179d0f480687284a04e33a7e
Joshua Ketor <joshuaketor@proton.me>
2024-04-03 00:08:02 +0000

    Update: 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,19 +3,17 @@
 
 
 class Square:
-    """square"""
+    """A square"""
 
     def __init__(self, size=0):
         """Constructor"""
 
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
-        is size < 0:
+        if size < 0:
             raise ValueError('size must be >= 0')
-
         self.__size = size
 
     def area(self):
         """Area of a square"""
-
         return self.__size ** 2
