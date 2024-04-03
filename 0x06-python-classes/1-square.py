599e309926fea0f49b6f54cba6d65c590637a88d
Joshua Ketor <joshuaketor@proton.me>
2024-04-03 17:22:39 +0000

    Add: 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module with a square class"""
+
+
+class Square:
+    """A square class"""
+
+    def __init__(self, size):
+        """Instatiation method
+        Args:
+            self (obj): self reference to instance
+            size: size of square
+        """
+
+        self.__size = size
