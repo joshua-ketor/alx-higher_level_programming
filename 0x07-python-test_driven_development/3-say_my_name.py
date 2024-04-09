da1092167905fa45b3291111717e1536a6a93e3c
Joshua Ketor <joshuaketor@proton.me>
2024-04-09 16:28:51 +0000

    Add: 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for say_my_name function"""
+
+def say_my_name(first_name, last_name=""):
+    """Function that prints `My name is <first name> <last name>
+    Args:
+        first_name (str): First name
+        last_name (str, optional): Last name, defaults to empty string
+
+    Raises:
+        TypeError: if first_name or last_name is not a string
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
