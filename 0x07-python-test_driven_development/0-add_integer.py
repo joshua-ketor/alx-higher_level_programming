b805c45f948394153ba1ba4d72460e9f14c48739
Joshua Ketor <joshuaketor@proton.me>
2024-04-09 08:25:01 +0000

    Add: 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Module with add function"""
+def add_integer(a, b=98):
+	"""Adds two integers
+	Args:
+		a (int): The first integer
+		b (int, optional): The second integer. Defaults to 98
+
+	Returns:
+		An integer: the addtion of a and b
+
+	Raises:
+		TypeError: if either `a` or `b` is not an integer
+	"""
+	if type(a) not in [int, float]:
+		raise TypeError("a must be an integer")
+	if type(b) not in [int, float]:
+		raise TypeError("b must be an integer")
+	return int(a + b)
