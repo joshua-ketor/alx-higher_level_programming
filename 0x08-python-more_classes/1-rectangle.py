bba0e65db575293555e51755c373d067c8950470
Joshua Ketor <joshuaketor@proton.me>
2024-04-11 03:16:05 +0000

    Add: 1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Module for Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle Class"""
+    def __init__(self, width=0, height=0):
+        """Constructor
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """width getter
+        Returns: the rectangle width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter
+        Raises:
+            TypeError: if width is not an integer
+            ValueError: if width is less than 0
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter
+        Returns: the rectangle height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter
+        Raises:
+            TypeError: if height is not an integer
+            ValueError: if height is less than 0
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
