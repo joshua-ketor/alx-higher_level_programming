4b0e0138ad5c5ed7e97422e621cb5152ec3f68a7
Joshua Ketor <joshuaketor@proton.me>
2024-04-16 23:43:34 +0000

    Add: 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module with load_from_json_file function"""
+import json
+
+
+def load_from_json_file(filename):
+    """Create an Object from a JSON file"""
+    with open(filename) as file:
+        return json.load(file)
