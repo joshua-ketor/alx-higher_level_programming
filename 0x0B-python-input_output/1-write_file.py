37c7ddd41017d377c28a12afe7a98ea36eb04c14
Joshua Ketor <joshuaketor@proton.me>
2024-04-16 23:25:56 +0000

    Add: 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module with write_file function"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and
+    returns the number of characters written"""
+    with open(filename, mode="w", encoding="utf-8") as file:
+        return file.write(text)
