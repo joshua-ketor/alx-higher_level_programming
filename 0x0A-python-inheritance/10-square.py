9d39833dfe825aafb6e8bd28cd5fdeea813bf16b
Joshua Ketor <joshuaketor@proton.me>
2024-04-16 03:26:22 +0000

    Add: 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Child class square and parent class Rectangle"""
+    def __init__(self, size):
+        """Constructor"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
