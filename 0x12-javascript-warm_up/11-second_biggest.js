c1e0f8ffa52d09675d5782c5512baba7534d3373
Joshua Ketor <joshuaketor@proton.me>
2024-06-19 03:20:22 +0000

    Update: 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,12 +4,11 @@ const len = args.length;
 let i, first, second;
 
 first = second = 0;
-for (i = 2; i < len; i++)
-{
+for (i = 2; i < len; i++) {
   if (args[i] > first) {
     second = first;
     first = args[i];
-  } else if (args[i] != first && args[i] > second) {
+  } else if (args[i] !== first && args[i] > second) {
     second = args[i];
   }
 }
