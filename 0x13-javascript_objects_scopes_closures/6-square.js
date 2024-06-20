f1cccacbaab38038e1d1890e0bd33720fb04e2da
Joshua Ketor <joshuaketor@proton.me>
2024-06-20 02:12:52 +0000

    Update: 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,13 +4,12 @@ const GreaterSquare = require('./5-square.js');
 class Square extends GreaterSquare {
   constructor (size) {
     super(size);
-    this.size = size;
   }
 
   charPrint (c) {
     if (c) {
-      for (let i = 0; i < this.size; i++) {
-        console.log('C'.repeat(this.size));
+      for (let i = 0; i < this.height; i++) {
+        console.log('C'.repeat(this.height));
       }
     } else {
       super.print();
